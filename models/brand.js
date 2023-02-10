09eb23d0df3842ca18e365e5215d390ad419ba7f
Nelly <$>
2023-02-09 20:40:24 -0500

    created brand schema

---
+++ models/brand.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Brand = new Schema(
+    {
+      brandName: { type: String, required: true },
+      brandLogo: { type: String, required: true }
+    },
+    { timestamps: true }
+  )
+
+
+module.exports = mongoose.model('Brand', Brand)
