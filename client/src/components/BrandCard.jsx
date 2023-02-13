9baa7ae37342016598476df65857a6e2c72fe5ae
Nelly <$>
2023-02-13 12:20:08 -0500

    created brand card component

---
+++ client/src/components/BrandCard.jsx
@@ -0,0 +1,13 @@
+const BrandCar = (props) => {
+    return (
+        <div className="card brand-card" onclick={props.onClick}>
+            <div className="img-wrapper">
+                <img alt="brand img" src={props.imag} />
+            </div>
+            <div className="info-wrapper flex-col">
+                <h3>{props.brand}</h3>
+            </div>
+        </div>
+    )
+}
+export default BrandCar
--- client/src/components/SneakerCard.jsx
+++ client/src/components/SneakerCard.jsx
@@ -1,16 +1,17 @@
 const SneakerCard = (props) => {
 
     return (
-        <div className="card sneaker-card">
+        <div className="card sneaker-card" onClick={props.onClick}>
             <div className="img-wrapper">
                 <img alt="sneaker img" src={props.image} />
             </div>
-            <div className="info-wrapper">
+            <div className="info-wrapper flex-col">
                 <h4>{props.name}</h4>
                 <h5>`${props.price}`</h5>
             </div>
         </div>
     )
 }
+// onclick => view more details 
 
 export default SneakerCard
