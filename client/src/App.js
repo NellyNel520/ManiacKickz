c4e8a79ed59e059868e6101102b94d2e52edc455
Nelly <$>
2023-02-13 18:33:22 -0500

    created route for viewing sneakers by brand

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
 import Products from './pages/Products';
 import Home from './pages/Home';
+import ViewSneakersByBrand from './pages/ViewSneakersByBrand';
 // import Footer from './components/Footer';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="products" element={ <Products />} />
+        <Route path="view/sneakers/:brandId" element={<ViewSneakersByBrand />} />
       </Routes>
       </main>
       {/* footer add later */}
