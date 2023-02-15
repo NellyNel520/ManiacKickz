05e8634711f303e59926b1d1fe71266722df557a
Nelly <$>
2023-02-15 00:21:13 -0500

    added form state and handle change function to form as well as added all form feilds and labels

--- client/src/components/AddSneakerForm.jsx
+++ client/src/components/AddSneakerForm.jsx
@@ -6,12 +6,32 @@ const AddSneakerForm = (props) => {
         issueType: '',
         subject: '',
         message: ''
-      }
-      const [formState, setFormState] = useState(initialState)
+    }
+    const [formState, setFormState] = useState(initialState)
+
+    const handleChange = (event) => {
+	    setFormState({...formState, [event.target.id]: event.target.value })
+	}
     
 
   return (
     <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        <label htmlFor="brandName">Brand:</label>
+        <input type="text" id="brandName" />
+        <label htmlFor="image">Image:</label>
+        <input type="text" id="image" />
+        <label htmlFor="colorWay">Colorway:</label>
+        <input type="text" id="colorWay" />
+        <label htmlFor="releaseDate">Release Date:</label>
+        <input type="text" id="releaseDate" />
+        <label htmlFor="price">Price: $</label>
+        <input type="text" id="price" />
+        <label htmlFor="styleCode">Style Code:</label>
+        <input type="text" id="styleCode" />
+
+
         <button type='submit'>Submit</button>
     </form>
   )
