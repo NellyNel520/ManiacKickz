e3c94cc3f5b4ecdc23b0e5594de5609e3103c820
Nelly <$>
2023-02-14 21:52:43 -0500

    organized data in sneaker details to render correctly and added a back button to go back to the products page

--- client/src/components/SneakerDetails.jsx
+++ client/src/components/SneakerDetails.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import { BASE_URL } from '../global'
+import moment from 'moment';
+import { Link } from 'react-router-dom'
 
 const SneakerDetails = (props) => {
     const [sneaker, setSneaker] = useState('')
@@ -21,18 +23,35 @@ const SneakerDetails = (props) => {
     }, [id])
     console.log(sneaker)
 
+    let releaseDate = moment(sneaker.releaseDate).format('MMM DD, YYYY')
+
 
   return sneaker ? (
     <div className='details'>
         <h1>{sneaker.name}</h1>
-        <h3>{sneaker.releaseDate}</h3>
+        <h3>{releaseDate}</h3>
         <div className='img-container'>
             <img src={sneaker.image} alt={sneaker.name} />
         </div>
-        <div className='info-wrapper'>
-            <h3></h3>
+        <div className='sneakerInfo-wrapper'>
+            <div className='wrapper2'>
+                    <div><h3>Name</h3></div>
+                    <div><h3>{sneaker.name}</h3></div>
+                    <div><h3>Colorway</h3></div>
+                    <div><h3>{sneaker.colorWay}</h3></div>
+                    <div><h3>Release Date</h3></div>
+                    <div><h3>{releaseDate}</h3></div>
+                    <div><h3>Style Code</h3></div>
+                    <div><h3>{sneaker.styleCode}</h3></div>
+                    <div><h3>Retail Price</h3></div>
+                    <div><h3>${sneaker.price}.00</h3></div>
+            </div>
         </div>
-
+        <div>
+        <Link to={'/sneakers'}>
+            <button>Back</button>
+        </Link>
+        </div>
     </div>
   ) : null
 }
