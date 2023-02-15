7ec6d359df9e47bf1dca0efdadae76932f46082e
Nelly <$>
2023-02-15 00:44:49 -0500

    connected each values attribute to its corresponding property in the form state

--- client/src/components/AddSneakerForm.jsx
+++ client/src/components/AddSneakerForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from '../global'
 
 const AddSneakerForm = (props) => {
     const initialState = {
@@ -12,24 +14,74 @@ const AddSneakerForm = (props) => {
     const handleChange = (event) => {
 	    setFormState({...formState, [event.target.id]: event.target.value })
 	}
+
+    const handleSubmit = async (event) => {
+		event.preventDefault()
+		await axios.post(`${BASE_URL}sneaker`, formState)
+		setFormState(initialState)
+		props.createSneaker()
+	}
     
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" />
+        <input 
+        type="text" 
+        id="name" 
+        onChange={handleChange}
+        value={formState.name}
+        />
         <label htmlFor="brandName">Brand:</label>
-        <input type="text" id="brandName" />
+        <input 
+        type="text" 
+        id="brandName" 
+        onChange={handleChange}
+        value={formState.brandName}
+        />
         <label htmlFor="image">Image:</label>
-        <input type="text" id="image" />
+        <input 
+        type="text" 
+        id="image" 
+        onChange={handleChange}
+        value={formState.image}
+        />
         <label htmlFor="colorWay">Colorway:</label>
-        <input type="text" id="colorWay" />
+        <input 
+        type="text" 
+        id="colorWay" 
+        onChange={handleChange}
+        value={formState.colorWay}
+        />
         <label htmlFor="releaseDate">Release Date:</label>
-        <input type="text" id="releaseDate" />
+        <input 
+        type="text" 
+        id="releaseDate" 
+        onChange={handleChange}
+        value={formState.releaseDate}
+        />
         <label htmlFor="price">Price: $</label>
-        <input type="text" id="price" />
+        <input 
+        type="text" 
+        id="price" 
+        onChange={handleChange}
+        value={formState.price}
+        />
         <label htmlFor="styleCode">Style Code:</label>
-        <input type="text" id="styleCode" />
+        <input 
+        type="text" 
+        id="styleCode" 
+        onChange={handleChange}
+        value={formState.styleCode}
+        />
+
+        <label htmlFor="sizes">Size(s):</label>
+        <input 
+        type="text" 
+        id="sizes" 
+        onChange={handleChange}
+        value={formState.sizes}
+        />
 
 
         <button type='submit'>Submit</button>
