84baa707625de4a635ab21803b14622528d3fa17
Nelly <$>
2023-02-16 20:19:19 -0500

    created stlye sheet for form and completed styling for add sneaker form

--- client/src/components/AddSneakerForm.jsx
+++ client/src/components/AddSneakerForm.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../global'
 import { useNavigate } from "react-router-dom"
+import '../styles/Form.css'
 
 
 const AddSneakerForm = (props) => {
@@ -35,68 +36,76 @@ const AddSneakerForm = (props) => {
     
 
   return (
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input 
-        type="text" 
-        id="name" 
-        onChange={handleChange}
-        value={formState.name}
-        />
-        <label htmlFor="brandName">Brand:</label>
-        <input 
-        type="text" 
-        id="brandName" 
-        onChange={handleChange}
-        value={formState.brandName}
-        />
-        <label htmlFor="image">Image:</label>
-        <input 
-        type="text" 
-        id="image" 
-        onChange={handleChange}
-        value={formState.image}
-        />
-        <label htmlFor="colorWay">Colorway:</label>
-        <input 
-        type="text" 
-        id="colorWay" 
-        onChange={handleChange}
-        value={formState.colorWay}
-        />
-        <label htmlFor="releaseDate">Release Date:</label>
-        <input 
-        type="text" 
-        id="releaseDate" 
-        onChange={handleChange}
-        value={formState.releaseDate}
-        />
-        <label htmlFor="price">Price: $</label>
-        <input 
-        type="number" 
-        id="price" 
-        onChange={handleChange}
-        value={formState.price}
-        />
-        <label htmlFor="styleCode">Style Code:</label>
-        <input 
-        type="text" 
-        id="styleCode" 
-        onChange={handleChange}
-        value={formState.styleCode}
-        />
+    <div className='container'>
+       <div className='wrapper'>
+          <div className='top'> Add Listing </div>
+          <div className='bottom' aria-hidden="true">  Add Listing</div>
+      </div>
+    <div className='form-container'>
+      <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input 
+          type="text" 
+          id="name" 
+          onChange={handleChange}
+          value={formState.name}
+          />
+          <label htmlFor="brandName">Brand:</label>
+          <input 
+          type="text" 
+          id="brandName" 
+          onChange={handleChange}
+          value={formState.brandName}
+          />
+          <label htmlFor="image">Image:</label>
+          <input 
+          type="text" 
+          id="image" 
+          onChange={handleChange}
+          value={formState.image}
+          />
+          <label htmlFor="colorWay">Colorway:</label>
+          <input 
+          type="text" 
+          id="colorWay" 
+          onChange={handleChange}
+          value={formState.colorWay}
+          />
+          <label htmlFor="releaseDate">Release Date:</label>
+          <input 
+          type="text" 
+          id="releaseDate" 
+          onChange={handleChange}
+          value={formState.releaseDate}
+          />
+          <label htmlFor="price">Price: $</label>
+          <input 
+          type="number" 
+          id="price" 
+          onChange={handleChange}
+          value={formState.price}
+          />
+          <label htmlFor="styleCode">Style Code:</label>
+          <input 
+          type="text" 
+          id="styleCode" 
+          onChange={handleChange}
+          value={formState.styleCode}
+          />
 
-        <label htmlFor="sizes">Size(s):</label>
-        <input 
-        type="text" 
-        id="sizes" 
-        onChange={handleChange}
-        value={formState.sizes}
-        />
+          <label htmlFor="sizes">Size(s):</label>
+          <input 
+          type="text" 
+          id="sizes" 
+          onChange={handleChange}
+          value={formState.sizes}
+          />
 
 
-        <button type='submit'>Submit</button>
-    </form>
+      </form>
+          <button type='submit'>Submit</button>
+      </div>
+    </div>
   )
 }
 
